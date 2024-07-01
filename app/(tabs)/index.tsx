95940e472cc048431a262e009c35e698962a557d
Chris <comosjay@gmail.com>
2024-07-01 11:54:22 -0700

    testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">hi</ThemedText>
+        <ThemedText type="title">hi This is a test!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
